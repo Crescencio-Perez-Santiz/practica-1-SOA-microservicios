16a36f99d9bea5bece77536b70841c68a3ca48d4
Crescens-Henry <crescencio.ps.code@gmail.com>
2024-05-14 19:36:54 -0600

    feat: Agregar proxy para la ruta '/api/v1/orders'
    Se agregó un proxy para la ruta '/api/v1/orders' que redirige las solicitudes a 'http://localhost:3002'. Esto permite que el servicio de pedidos se comunique correctamente con el servicio principal. implementacion de el servicio de ordenes

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,10 @@ const signale = new Signale();
 dotenv.config();
 
 app.use(morgan('dev'));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const GATEWAY = process.env.SERVICE_NAME;
 
-// app.use('/api/v1/orders',proxy('http://localhost:3002'));
+app.use('/api/v1/orders',proxy('http://localhost:3002'));
 app.use('/api/v1/products',proxy('http://localhost:3001'));
 
 // rutas de auth
